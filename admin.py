f13a0c92ec9797f22f28cd93b0a88943e5299ac8
KrupaPatel-26 <krupa260404@gmail.com>
2024-09-28 14:41:40 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost,Comment,Like
-# Register your models here.
-
-
-admin.site.register(BlogPost)
-admin.site.register(Comment)
-admin.site.register(Like)
